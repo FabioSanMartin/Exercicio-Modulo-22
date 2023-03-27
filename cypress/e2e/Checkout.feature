b89326de865c1fa4e3a8f183c395672f73305c42
fabio Bruno <messinasm88@gmail.com>
2023-03-27 00:05:07 -0300

    adicionado mais uma feature

--- cypress/e2e/Checkout.feature
+++ cypress/e2e/Checkout.feature
@@ -5,6 +5,4 @@ Given Eu acesse a página da EBAC Store com e-mail "<user>" e a senha "<pass>"
 When Eu adicione um produto ao carrinho e complete o cadastro
 Then A tela com a mensagem com o pedido deve aparecer
 
-
-
     Feature Description
---
+++ cypress/e2e/Checkout.steps.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+const data = require('../fixtures/data.json')
+
+Given("Eu acesse a página da EBAC Store com meu usuario {string} e a senha {string}", (usuario, senha) => {
+    cy.login(data.usuario, data.senha)
+})
+
+When('Eu adicione um produto ao carrinho e complete o cadastro', ()=> {
+    cy.addIten()
+    cy.checkout(data.firstName, data.lastName, data.country, data.address1, data.city, data.city, data.state, data.postCode, data.phone, data.email)
+})
+
+Then('A tela com a mensagem com o pedido deve aparecer', ()=> {
+    cy.checkoutConfirm()
+})
